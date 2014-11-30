65efb1f097fac62fb192995cf357f4162d37aa07
zackerous <cenejenko@gmail.com>
2014-11-30 10:04:53 +0100

    Update extension.js

--- extension.js
+++ extension.js
@@ -53,11 +53,11 @@
     //Change the bots default settings and make sure they are loaded on launch
 
     localStorage.setItem("basicBotsettings", JSON.stringify({
-        botName: "basicBot",
+        botName: "InfectedBot",
         language: "english",
         chatLink: "https://rawgit.com/Yemasthui/basicBot/master/lang/en.json",
-        maximumAfk: 120,
-        afkRemoval: true,
+        maximumAfk: 95000,
+        afkRemoval: false,
         maximumDc: 60,
         bouncerPlus: true,
         lockdownEnabled: false,
